73631732cbcd2e61edc6e4c8394b7ab4a7c6d7c6
Dan <im.daniel.min@proton.me>
2025-10-04 22:50:06 -0400

    tichozrout: uncomment. warpdrive: initial

--- Scripts/tichozrout.sh
+++ Scripts/tichozrout.sh
@@ -25,9 +25,9 @@ process_file() {
     mkdir -p "$outdir"
 
     # liberal silence removal: anything quieter than -30dB for 0.3s
-    # ffmpeg -hide_banner -loglevel error -y -i "$infile" \
-        # -af "silenceremove=start_periods=1:start_threshold=-30dB:start_silence=0.3:stop_periods=-1:stop_threshold=-30dB:stop_silence=0.3" \
-        # "$outfile"
+    ffmpeg -hide_banner -loglevel error -y -i "$infile" \
+        -af "silenceremove=start_periods=1:start_threshold=-30dB:start_silence=0.3:stop_periods=-1:stop_threshold=-30dB:stop_silence=0.3" \
+        "$outfile"
 
     echo "Processed: $infile -> $outfile"
 }
