24de424428c122fed3d0d30725f3b419ebddd65d
Dan <im.daniel.min@proton.me>
2026-01-17 15:29:58 -0500

    notepad fix

--- dot/fish/config.fish
+++ dot/fish/config.fish
@@ -191,25 +191,14 @@ alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 alias cls='echo "" > "$HOME/.local/share/fish/fish_history"'
 
 # abbreviations
-abbr -a 'upd' 'sudo pacman -Syyu && yay -Syyu && doom upgrade && flatpak update'
+# abbr -a 'upd' 'sudo pacman -Syyu && yay -Syyu && doom upgrade && flatpak update'
+abbr -a 'upd' 'sudo pacman -Syyu && doom upgrade && flatpak update'
 
-abbr -a 'gente' 'yt-dlp -f "bv*+ba/b" \
-    --embed-metadata --embed-thumbnail \
-    --write-subs \
-    --write-auto-subs \
-    --sub-langs es \
-    --embed-subs \
-    --sponsorblock-remove all \
-    --remote-components ejs:github \
-    '
-
-abbr -a 'video' 'yt-dlp -f "bestvideo[height<=360][vcodec^=avc1]+bestaudio[ext=m4a]/best[height<=360]" \
-       -S "height:360,+size,+vbr:800,+abr:96" \
-       --merge-output-format mp4\
-       --embed-metadata --embed-thumbnail \
-       --write-auto-subs --sub-langs "en" \
+abbr -a 'video' 'yt-dlp -f "bestvideo[height<=480]+bestaudio/best" \
+       --embed-metadata \
        --restrict-filenames --continue \
-       --retries 10'
+       --sponsorblock-remove all \
+       --remote-components ejs:github'
 
 abbr -a 'music' 'yt-dlp -f "bestaudio/best" -x --audio-format flac --embed-thumbnail --embed-metadata --no-write-description --no-write-info-json --no-write-comments'
 
